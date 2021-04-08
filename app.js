b9d49cfa5e5160988fda25e7e532eae3ba507082
unknown <Medici305>
2021-04-08 16:24:31 +0100

    4. Added advanced search menu and dark-mode functionality.

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 // Variables
 const toggleButton = document.querySelector(".toggle-btn");
+const searchBar = document.querySelector(".search-bar");
+const filterIcon = searchBar.querySelector(".fa-filter");
 
 // Functions
 function activateDarkMode(e) {
+  document.body.classList.toggle("dark_mode");
   const moonIcon = toggleButton.parentElement.querySelector("img");
   /* Toggle button functionality and change moon/sun icon */
   if (!e.target.classList.contains("active")) {
     toggleButton.classList.add("active");
     moonIcon.src = "../images/desktop/icon-sun.svg";
-    document.body.style.background = "#121721";
   } else {
     toggleButton.classList.remove("active");
     moonIcon.src = "../images/desktop/icon-moon.svg";
-    document.body.style.background = "";
+  }
+}
+
+function advancedSearch() {
+  const box = document.querySelector(".box");
+  if (!box.classList.contains("active")) {
+    box.classList.add("active");
+    box.style.display = "flex";
+  } else {
+    box.classList.remove("active");
+    box.style.display = "";
   }
 }
 
 // EventListeners
 toggleButton.addEventListener("click", activateDarkMode);
+filterIcon.addEventListener("click", advancedSearch);
